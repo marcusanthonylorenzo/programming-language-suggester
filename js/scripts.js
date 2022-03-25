58c79a319c9e853f74ff77305185c0077e894fc2
Marcus <marcanthonyzpadilla@gmail.com>
2022-03-25 10:54:51 -0700

    add if/else if/else conditions

--- js/scripts.js
+++ js/scripts.js
@@ -6,18 +6,35 @@ function totalValue (input1, input2, input3, input4, input5, input6) {
 //UX UI Logic
 $(document).ready(function () {
   $("#form").submit(function(event) {
+
+
+  //primary selectors
+    const coffeeRadioValue = parseInt($("input:radio[name=coffee]:checked").val());
+    const painRadioValue= parseInt($("input:radio[name=pain]:checked").val());
+    const roomRadioValue = parseInt($("input:radio[name=room]:checked").val());
+    const pizzaRadioValue = parseInt($("input:radio[name=pizza]:checked").val());
+    const carRadioValue = parseInt($("input:radio[name=car]:checked").val());
+    const bookRadioValue = parseInt($("input:radio[name=book]:checked").val());
+    const button = $("#button");
+    const display = $("#display");
+
+    let totalValueResult = totalValue(coffeeRadioValue, painRadioValue, roomRadioValue, pizzaRadioValue, carRadioValue, bookRadioValue);
+
+
+    button.click(function () {
+      if (totalValueResult <= 20) {
+        display.text("1")
+        display.show();
+      } else if (totalValueResult >20 && totalValueResult < 30) {
+        display.text("2")
+        display.show();
+      } else {
+        display.text("3")
+        display.show();
+      };
+    })
     event.preventDefault();
-
-  const coffeeRadioValue = parseInt($("input:radio[name=coffee]:checked").val());
-  const painRadioValue= parseInt($("input:radio[name=pain]:checked").val());
-  const roomRadioValue = parseInt($("input:radio[name=room]:checked").val());
-  const pizzaRadioValue = parseInt($("input:radio[name=pizza]:checked").val());
-  const carRadioValue = parseInt($("input:radio[name=car]:checked").val());
-  const bookRadioValue = parseInt($("input:radio[name=book]:checked").val());
-
-  const totalValueResult = totalValue(coffeeRadioValue, painRadioValue, roomRadioValue, pizzaRadioValue, carRadioValue, bookRadioValue);
-
-  console.log(totalValueResult);
+    console.log(totalValueResult);
 
   });
 });
