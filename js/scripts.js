21c6a61b07237bdd7ed64ced4343d0ed8d4e05fa
Marcus <marcanthonyzpadilla@gmail.com>
2022-03-25 23:11:37 -0700

    add better modal response

--- js/scripts.js
+++ js/scripts.js
@@ -14,41 +14,50 @@ $(document).ready(function () {
     const pizzaRadioValue = parseInt($("input:radio[name=pizza]:checked").val());
     const carRadioValue = parseInt($("input:radio[name=car]:checked").val());
     const bookRadioValue = parseInt($("input:radio[name=book]:checked").val());
+    const container = $(".container");
     const display = $("#display");
     const outputBox = $(".outputBox");
     //function
     let totalValueResult = totalValue(coffeeRadioValue, painRadioValue, roomRadioValue, pizzaRadioValue, carRadioValue, bookRadioValue);
 
     if (totalValueResult <26) {
-      display.html("<div class='closeBox'><button class='close'>X</button></div><div class='popUp'><img src='css/images/arnieImg.png' id='arnieImg'><span class='popUpText'><h4> Congrats! You got <strong>Arnold C</strong></h4> <p><strong>Yes, it's a real language, apparently.</strong> Named after the Governator himself, you don't seem to mind anything too complicated; infact, you may get by in life with basic syntax and lots of yelling. Enjoy pumping weights and pumping out mindless code!</p></span></div>").show();
+      display.html("<div class='closeBox'><button class='close'>X</button></div><div class='popUp'><img src='css/images/arnieImg.png' id='arnieImg' alt='arnold'><span class='popUpText'><h4> Congrats! You got <strong>Arnold C</strong></h4> <p><strong>Yes, it's a real language, apparently.</strong> Named after the Governator himself, you don't seem to mind anything too complicated; infact, you may get by in life with basic syntax and lots of yelling. Enjoy pumping weights and pumping out mindless code!</p></span></div>").show();
       outputBox.show();
       //declare variable locally not globally for button functionality.
+      container.addClass("bgFade");
       const closeButton = $(".close");
       closeButton.click(function() {
+        container.removeClass("bgFade");
         outputBox.hide();
       });
       $("#form").trigger("reset");
     } else if (totalValueResult >=26 && totalValueResult <= 32) {
-      display.html("<div class='closeBox'><button class='close'>X</button></div><div class='popUp'><img src='css/images/apple.png' id='apple'><span class='popUpText'><h4> Congrats! You got <strong>Swift</strong></h4> <p><strong>A step-child of the Apple conglomerate!</strong> So, you might be low-maintainence and prefer something reliable, <em>or</em> you might be high-maintainence and slightly pretentious, but you enjoy the finer things in life. Either way: you probably love coffee, have some sense of minimalism, and love you some wholesome, quality, and straight-forward (albeit exclusive) code.</p></span></div>").show();
+      display.html("<div class='closeBox'><button class='close'>X</button></div><div class='popUp'><img src='css/images/apple.png' id='apple' alt='swift'><span class='popUpText'><h4> Congrats! You got <strong>Swift</strong></h4> <p><strong>A step-child of the Apple conglomerate!</strong> So, you might be low-maintainence and prefer something reliable, <em>or</em> you might be high-maintainence and slightly pretentious, but you enjoy the finer things in life. Either way: you probably love coffee, have some sense of minimalism, and love you some wholesome, quality, and straight-forward (albeit exclusive) code.</p></span></div>").show();
       outputBox.show();
+      container.addClass("bgFade");
       const closeButton = $(".close");
       closeButton.click(function() {
+        container.removeClass("bgFade");
         outputBox.hide();
       });
       $("#form").trigger("reset");
     } else if (totalValueResult > 32) {
-      display.html("<div class='closeBox'><button class='close'>X</button></div><div class='popUp'><img src='css/images/hackermanImg.jpg' id='hackermanImg'><span class='popUpText'><h4> Congrats! You got <strong>C++</strong></h4> <p><strong>Good luck living</strong>. You might like drama, or pain, or stress - or maybe you're just <em>hardcore</em>. The truth is: I couldn't tell you much about C++ or older languages like Java, but I always hear the murmurings of how obnoxiously complex they are. My memory is as bad as it is, so I couldn't imagine memory management for someone else, let alone a computer and it's language.</p><p>Your destiny is probably that of a tech engineer.</p></span></div>").show();
+      display.html("<div class='closeBox'><button class='close'>X</button></div><div class='popUp'><img src='css/images/hackermanImg.jpg' id='hackermanImg' alt='hackerman'><span class='popUpText'><h4> Congrats! You got <strong>C++</strong></h4> <p><strong>Good luck living</strong>. You might like drama, or pain, or stress - or maybe you're just <em>hardcore</em>. The truth is: I couldn't tell you much about C++ or older languages like Java, but I always hear the murmurings of how obnoxiously complex they are. My memory is as bad as it is, so I couldn't imagine memory management for someone else, let alone a computer and it's language.</p><p>Your destiny is probably that of a tech engineer.</p></span></div>").show();
       outputBox.show();
+      container.addClass("bgFade");
       const closeButton = $(".close");
       closeButton.click(function() {
+        container.removeClass("bgFade");
         outputBox.hide();
       });
       $("#form").trigger("reset");
     } else {
-      display.html("<div class='closeBox'><button class='close'>X</button></div><div class='popUp' id='error'><h4>Don't be lazy!</h4> <p>Please make sure you've actually clicked all the buttons.</p></span><img src='css/images/steve.jpg' id='steve'><p id='parkour'>'parkour'</p></div>").show();
+      display.html("<div class='closeBox'><button class='close'>X</button></div><div class='popUp' id='error'><h4>Uhh...Don't be lazy!</h4><img src='css/images/catLoading.jpg' id='cat' alt='cat'><p>Please make sure you've actually clicked all the buttons.</p></span><img src='css/images/steve.jpg' id='steve' alt='steve'><p id='parkour'>'parkour'</p></div>").show();
       outputBox.show();
+      container.addClass("bgFade");
       const closeButton = $(".close");
       closeButton.click(function() {
+        container.removeClass("bgFade");
         outputBox.hide();
       });
     };
