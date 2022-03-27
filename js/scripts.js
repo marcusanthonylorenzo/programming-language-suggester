24b1f2424d64b58297be68006e2fb342c4077e3f
Marcus <marcanthonyzpadilla@gmail.com>
2022-03-27 09:03:32 -0700

    add media query: landscape

--- js/scripts.js
+++ js/scripts.js
@@ -19,11 +19,12 @@ $(document).ready(function () {
     const display = $("#display");
     const outputBox = $(".outputBox");
 
-    //function
+    //non-selector expressions
     let totalValueResult = totalValue(coffeeRadioValue, painRadioValue, roomRadioValue, pizzaRadioValue, carRadioValue, bookRadioValue);
+    let closePopUpBar = "<div class='closeBox'><button class='close'>X</button></div>";
 
-    if (totalValueResult <19) {
-      display.html("<div class='closeBox'><button class='close'>X</button></div><div class='popUp'><img src='css/images/arnieImg.png' id='arnieImg' alt='arnold'><span class='popUpText'><h4> Congrats! You got <strong>Arnold C</strong></h4> <p><strong>Yes, it's a real language, apparently.</strong> Named after the Governator himself, you don't seem to mind anything too complicated; infact, you may get by in life with basic syntax and lots of yelling. Enjoy pumping weights and pumping out mindless code!</p></span></div>").show();
+    if (totalValueResult <22) {
+      display.html(closePopUpBar + "<div class='popUp'><img src='css/images/arnieImg.png' id='arnieImg' alt='arnold'><span class='popUpText'><h4> Congrats! You got <strong>Arnold C</strong></h4> <p><strong>Yes, it's a real language, apparently.</strong> Named after the Governator himself, you don't seem to mind anything too complicated; infact, you may get by in life with basic syntax and lots of yelling. Enjoy pumping weights and pumping out mindless code!</p></span></div>").show();
       outputBox.show();
       //declare variable locally not globally for button functionality.
       container.addClass("bgFade");
@@ -33,8 +34,8 @@ $(document).ready(function () {
         outputBox.hide();
       });
       $("#form").trigger("reset");
-    } else if (totalValueResult >=19 && totalValueResult < 24) {
-      display.html("<div class='closeBox'><button class='close'>X</button></div><div class='popUp'><img src='css/images/python.png' id='python' alt='python'><span class='popUpText'><h4> Congrats! You got <strong>Python</strong></h4> <p><strong>The language of the people!</strong> You know that one person who is friends with every one in high school? Just an 'all-around' lad? The one that you can 'hit-up' on a Wednesday night and is always down for a chill beverage in town? No extra drama, no wild stories...that's Python for you.</p><p>Python may not be the fastest, or the most specialized, and you may find some level of 'complacency' with it...but it's just a nice language to code. Maybe just as nice as <em>you</em>.</span></div>").show();
+    } else if (totalValueResult >=22 && totalValueResult < 24) {
+      display.html(closePopUpBar + "<div class='popUp'><img src='css/images/python.png' id='python' alt='python'><span class='popUpText'><h4> Congrats! You got <strong>Python</strong></h4> <p><strong>The language of the people!</strong> You know that one person who is friends with every one in high school? Just an 'all-around' lad? The one that you can 'hit-up' on a Wednesday night and is always down for a chill beverage in town? No extra drama, no wild stories...that's Python for you.</p><p>Python may not be the fastest, or the most specialized, and you may find some level of 'complacency' with it...but it's just a nice language to code. Maybe just as nice as <em>you</em>.</span></div>").show();
       outputBox.show();
       container.addClass("bgFade");
       const closeButton = $(".close");
@@ -44,7 +45,7 @@ $(document).ready(function () {
       });
       $("#form").trigger("reset");
     } else if (totalValueResult >=24 && totalValueResult <= 26) {
-      display.html("<div class='closeBox'><button class='close'>X</button></div><div class='popUp'><img src='css/images/apple.png' id='apple' alt='swift'><span class='popUpText'><h4> Congrats! You got <strong>Swift</strong></h4> <p><strong>A step-child of the Apple conglomerate!</strong> So, you might be low-maintainence and prefer something reliable, <em>or</em> you might be high-maintainence and slightly eliteist (iOS ftw!), but you enjoy the finer things in life. Either way: you probably love coffee, have some sense of minimalism, and love you some wholesome, quality, and straight-forward (albeit exclusive) code.</p></span></div>").show();
+      display.html(closePopUpBar + "<div class='popUp'><img src='css/images/swift.png' id='swift' alt='swift'><span class='popUpText'><h4> Congrats! You got <strong>Swift</strong></h4> <p><strong>A step-child of the Apple conglomerate!</strong> So, you might be low-maintainence and prefer something reliable, <em>or</em> you might be high-maintainence and slightly eliteist (iOS ftw!), but you enjoy the finer things in life. Either way: you probably love coffee, have some sense of minimalism, and love you some wholesome, quality, and straight-forward (albeit exclusive) code.</p></span></div>").show();
       outputBox.show();
       container.addClass("bgFade");
       const closeButton = $(".close");
@@ -54,7 +55,7 @@ $(document).ready(function () {
       });
       $("#form").trigger("reset");
     } else if (totalValueResult > 26) {
-      display.html("<div class='closeBox'><button class='close'>X</button></div><div class='popUp'><img src='css/images/hackermanImg.jpg' id='hackermanImg' alt='hackerman'><span class='popUpText'><h4> Congrats! You got <strong>C++</strong></h4> <p><strong>Good luck living</strong>. You might like drama, or pain, or stress - or maybe you're just <em>hardcore</em>. The truth is: I couldn't tell you much about C++ or older languages like Java, but I always hear the murmurings of how obnoxiously complex they are. My memory is as bad as it is, so I couldn't imagine memory management for someone else, let alone a computer and it's language.</p><p>Your destiny is probably that of a tech engineer.</p></span></div>").show();
+      display.html(closePopUpBar + "<div class='popUp'><img src='css/images/hackermanImg.jpg' id='hackermanImg' alt='hackerman'><span class='popUpText'><h4> Congrats! You got <strong>C++</strong></h4> <p><strong>Good luck living</strong>. You might like drama, or pain, or stress - or maybe you're just <em>hardcore</em>. The truth is: I couldn't tell you much about C++ or older languages like Java, but I always hear the murmurings of how obnoxiously complex they are. My memory is as bad as it is, so I couldn't imagine memory management for someone else, let alone a computer and it's language.</p><p>Your destiny is probably that of a tech engineer.</p></span></div>").show();
       outputBox.show();
       container.addClass("bgFade");
       const closeButton = $(".close");
@@ -64,7 +65,7 @@ $(document).ready(function () {
       });
       $("#form").trigger("reset");
     } else {
-      display.html("<div class='closeBox'><button class='close'>X</button></div><div class='popUp' id='error'><h4>Uhh...Don't be lazy!</h4><img src='css/images/catLoading.jpg' id='cat' alt='cat'><p>Please make sure you've actually clicked all the buttons.</p></span><img src='css/images/steve.jpg' id='steve' alt='steve'><p id='parkour'>'parkour'</p></div>").show();
+      display.html(closePopUpBar + "<div class='popUp' id='error'><h4>Uhh...Don't be lazy!</h4><img src='css/images/catLoading.jpg' id='cat' alt='cat'><p>Please make sure you've actually clicked all the buttons.</p></span><img src='css/images/steve.jpg' id='steve' alt='steve'><p id='parkour'>'parkour'</p></div>").show();
       outputBox.show();
       container.addClass("bgFade");
       const closeButton = $(".close");
